49fd9c3eb391a782996235b52e318bac9e2198bc
Lambere <krutysh.super@yandex.ru>
2024-09-07 09:46:16 +0200

    box

---
+++ src/test/java/ru/job4j/BoxTest.java
@@ -0,0 +1,63 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.assertj.Box;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+    @Test
+    void isThisBodyHas0Vertices() {
+        Box box = new Box(0, 10);
+        int numberOfVertices = box.getNumberOfVertices();
+        assertThat(numberOfVertices).isEqualTo(0);
+    }
+
+    @Test
+    void isThisBodyHas4Vertices() {
+        Box box = new Box(4,10);
+        int numbOfVertices = box.getNumberOfVertices();
+        assertThat(numbOfVertices).isEqualTo(4);
+    }
+
+    @Test
+    void isNotExist() {
+        Box box = new Box(3, 10);
+        boolean exist = box.isExist();
+        assertThat(exist).isFalse();
+    }
+
+    @Test
+    void isExist() {
+        Box box = new Box(4, 10);
+        boolean exist = box.isExist();
+        assertThat(exist).isTrue();
+    }
+
+    @Test
+    void getAreaOfTetrahedron() {
+        Box box = new Box(4, 10);
+        double area = box.getArea();
+        assertThat(area).isEqualTo(173.20508075688772);
+    }
+
+    @Test
+    void getAreaOfCube() {
+        Box box = new Box(8, 10);
+        double area = box.getArea();
+        assertThat(area).isEqualTo(600);
+    }
+}
