6450a401806ced572439043e4dc7a266e77cff72
Lambere <krutysh.super@yandex.ru>
2024-09-08 14:38:51 +0200

    box

--- src/test/java/ru/job4j/BoxTest.java
+++ src/test/java/ru/job4j/BoxTest.java
@@ -3,6 +3,7 @@ package ru.job4j;
 import org.junit.jupiter.api.Test;
 import ru.job4j.assertj.Box;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
 
 class BoxTest {
     @Test
@@ -23,14 +24,18 @@ class BoxTest {
     void isThisBodyHas0Vertices() {
         Box box = new Box(0, 10);
         int numberOfVertices = box.getNumberOfVertices();
-        assertThat(numberOfVertices).isEqualTo(0);
+        assertThat(numberOfVertices).isEqualTo(0)
+                .isGreaterThan(-1)
+                .isLessThan(1);
     }
 
     @Test
     void isThisBodyHas4Vertices() {
         Box box = new Box(4, 10);
         int numbOfVertices = box.getNumberOfVertices();
-        assertThat(numbOfVertices).isEqualTo(4);
+        assertThat(numbOfVertices).isEqualTo(4)
+                .isGreaterThan(3)
+                .isLessThan(5);
     }
 
     @Test
@@ -51,13 +56,15 @@ class BoxTest {
     void getAreaOfTetrahedron() {
         Box box = new Box(4, 10);
         double area = box.getArea();
-        assertThat(area).isEqualTo(173.20508075688772);
+        assertThat(area).isEqualTo(173.20508075688772, withPrecision(0.006d));
     }
 
     @Test
     void getAreaOfCube() {
         Box box = new Box(8, 10);
         double area = box.getArea();
-        assertThat(area).isEqualTo(600);
+        assertThat(area).isEqualTo(600)
+                .isGreaterThan(599)
+                .isLessThan(601);
     }
 }
