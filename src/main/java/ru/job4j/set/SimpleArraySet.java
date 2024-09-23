8dce34c8e9bdc9bd611eb6736a3a1604a3fecc10
Даниил <krutysh.super@yandex.ru>
2024-09-23 23:40:40 +0200

    2

---
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -0,0 +1,36 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.Iterator;
+
+public class SimpleArraySet<T> implements SimpleSet<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        for (int i = 0; i < set.size(); i++) {
+            if (set.get(i) == value) {
+                return false;
+            }
+        }
+        set.add(value);
+        return true;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        for (int i = 0; i < set.size(); i++) {
+            if ( set.get(i) == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
