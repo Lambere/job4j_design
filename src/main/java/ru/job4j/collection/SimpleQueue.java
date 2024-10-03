bb7a70bd8eaf397289014316aa7eff47e4984504
Даниил <krutysh.super@yandex.ru>
2024-10-03 11:41:24 +0200

    18

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,22 +1,25 @@
 package ru.job4j.collection;
 
-public class SimpleQueue<T> {
-    private final SimpleStack<T> input = new SimpleStack<>();
-    private final SimpleStack<T> output = new SimpleStack<>();
+import java.util.NoSuchElementException;
 
+public class SimpleQueue<T> {
+    private SimpleStack<T> input = new SimpleStack<>();
+    private final SimpleStack<T> output = new SimpleStack<>();
     private int size;
 
     public T poll() {
         int sizeCopy = size;
-        while (sizeCopy-- >= 2) {
-             input.push(output.pop());
+        while (--sizeCopy >= 0) {
+            input.push(output.pop());
         }
+
         size--;
-        return output.pop();
+        return input.pop();
     }
 
     public void push(T value) {
-        output.push(value);
         size++;
+        output.push(value);
     }
+
 }
