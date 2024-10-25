67a1917d55edb3bc1f19faa0c81ddb771a29740a
Даниил <krutysh.super@yandex.ru>
2024-10-25 21:11:42 +0200

    42

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -9,11 +10,30 @@ public class SimpleTree<E> implements Tree<E> {
         this.root = new Node<>(root);
     }
 
+    public boolean isBinary() {
+        return findByPredicate(x -> x.children.size() <= 2).isPresent();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (condition.test(element)) {
+                result = Optional.of(element);
+                break;
+            }
+            data.addAll(element.children);
+        }
+        return result;
+    }
+
     @Override
     public boolean add(E parent, E child) {
         Optional<Node<E>> findParent = findBy(parent);
         boolean result = false;
-        if (findBy(child).isEmpty() && findParent.isPresent()) {
+        if (isBinary() && findBy(child).isEmpty() && findParent.isPresent()) {
             findParent.get().children.add(new Node<>(child));
             result = true;
 
@@ -24,17 +44,6 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> result = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> element = data.poll();
-            if (element.value.equals(value)) {
-                result = Optional.of(element);
-                break;
-            }
-            data.addAll(element.children);
-        }
-        return result;
+        return findByPredicate(x -> x.value.equals(value));
     }
 }
