7ede0052c0c7f2b504c15a614c49c1a13ee2158b
Даниил <krutysh.super@yandex.ru>
2024-11-12 20:50:45 +0100

    43

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,30 @@
+package ru.job4j.question;
+
+import java.util.*;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted;
+        Map<Integer, String> prevMap = new HashMap<>();
+        for (User user : previous) {
+            prevMap.put(user.getId(), user.getName());
+        }
+
+        for (User user : current) {
+            String prevName = prevMap.remove(user.getId());
+            if (prevName == null) {
+                added++;
+            } else if (!prevName.equals(user.getName())) {
+                changed++;
+            }
+        }
+
+        deleted = prevMap.size();
+
+        return new Info(added, changed, deleted);
+    }
+}
+
