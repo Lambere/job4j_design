0b8c45f94ab67bcdca42769af18ed90c3c291258
Даниил <krutysh.super@yandex.ru>
2024-11-22 22:44:22 +0100

    55

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -2,25 +2,22 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.LinkedHashMap;
 
 public class Analysis {
 
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            LinkedHashMap<String, String> map = new LinkedHashMap<>();
             SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
             String finish = "99:99:99";
             while (reader.ready()) {
-                String[] second = reader.readLine().split(" ");
-                map.put(second[1], second[0]);
-            }
-            for (String a : map.keySet()) {
-                if (Integer.parseInt(map.get(a)) > 300 && sdf.parse(a).before(sdf.parse(finish))) {
-                    finish = a;
+                String[] a = reader.readLine().split(" ");
+                if (Integer.parseInt((a[0])) > 300) {
+                    if (sdf.parse(a[1]).before(sdf.parse(finish))) {
+                        finish = a[1];
+                    }
                 } else if (!finish.equals("99:99:99")) {
-                    writer.println(finish + ";" + a);
+                    writer.println(finish + ";" + a[1]);
                     finish = "99:99:99";
                 }
             }
