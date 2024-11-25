2ccd669ea34dc0132dba03a7153f997a00769728
Даниил <krutysh.super@yandex.ru>
2024-11-25 22:06:12 +0100

    56

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,61 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            String a = reader.readLine();
+            String[] values = a.split("=");
+            if (a.endsWith("=")) {
+                values[1] += "=";
+            }
+            if (values[1].isEmpty() || values[0].isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            if (values.length > 2) {
+                for (int i = 2; i < values.length; i++) {
+                    values[1] += "=" + values[i];
+                }
+            }
+                this.values.put(values[0], values[1]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+
+}
+
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "data\\pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithWrongFormat() {
+        String path = "data\\pair_wrong_format.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThatException();
+    }
+
+    @Test
+    void whenPairWithWrongFormat2() {
+        String path = "data\\pair_wrong_format_2.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr=");
+    }
+
+    @Test
+    void whenPairWithWrongFormat3() {
+        String path = "data\\pair_wrong_format_3.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr=1");
+    }
+
+}
