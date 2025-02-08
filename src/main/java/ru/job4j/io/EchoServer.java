22e683270a2b9dc0a957eeb9fba4afedeb4d4ca9
Даниил <krutysh.super@yandex.ru>
2025-02-08 18:56:54 +0100

    101

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 
 public class EchoServer {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
