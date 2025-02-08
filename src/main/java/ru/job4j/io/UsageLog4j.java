b3e1e6d32099192e1745b29586b67c69e2f4d6b5
Даниил <krutysh.super@yandex.ru>
2025-02-08 18:15:01 +0100

    99

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,26 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+        short b = 21;
+        long c = 2L;
+        double d = 12.2D;
+        byte f = 1;
+        float g = 33f;
+        char h = 'a';
+        boolean e = true;
+        int a = 1;
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        LOG.info(String.valueOf(a));
+        LOG.info(String.valueOf(b));
+        LOG.info(String.valueOf(c));
+        LOG.info(String.valueOf(d));
+        LOG.info(String.valueOf(e));
+        LOG.info(String.valueOf(f));
+        LOG.info(String.valueOf(g));
+        LOG.info(String.valueOf(h));
     }
 }
